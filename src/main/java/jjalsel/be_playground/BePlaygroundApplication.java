5fb11a2658ac1cac0b634b46455f84ac1fd8ed18
jjalseu <cjfwns966@gmail.com>
2024-09-24 21:36:43 +0900

    first commit

---
+++ src/main/java/jjalsel/be_playground/BePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package jjalsel.be_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BePlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BePlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jjalsel/be_playground/BePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package jjalsel.be_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BePlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
