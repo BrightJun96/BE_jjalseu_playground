5ad668e57036f49c8ff08203c375fde5b3db0c0a
jjalseu <cjfwns966@gmail.com>
2024-11-24 18:33:20 +0900

    - 퀴즈 응답 DTO level 필드 추가

--- src/main/java/jjalsel/be_playground/BePlaygroundApplication.java
+++ src/main/java/jjalsel/be_playground/BePlaygroundApplication.java
@@ -1,5 +1,6 @@
 package jjalsel.be_playground;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BePlaygroundApplication {
 
     public static void main(String[] args) {
+
+        // .env 파일 로드
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")  // .env 파일 경로
+                .load();
+
+        // 환경 변수를 시스템 속성에 설정
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         SpringApplication.run(BePlaygroundApplication.class, args);
     }
 
